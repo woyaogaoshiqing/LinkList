0eab4f6f0fb3e1eb844b3cf6bad6ca3a936b4963
woyaogaoshiqing <402824251@qq.com>
2023-12-20 10:15:08 +0800

    [修改]完成函数的形参，并在源文件定义

--- linklist.c
+++ linklist.c
@@ -4,31 +4,61 @@
 #include <stdio.h>
 
 
-int linklistInit( linklist **pList);
+int linklistInit( linklist **pList)
+{
+
+}
 
 /*头插*/
-int linklistheadInser(linklist *pList, ELEMENTTYPE val);
+int linklistheadInser(linklist *pList, ELEMENTTYPE val)
+{
+
+}
 
 /*尾插*/
-int linklisttaiInser(linklist *pList, ELEMENTTYPE val);
+int linklisttaiInser(linklist *pList, ELEMENTTYPE val)
+{
+
+}
 
 /*链表指定位置插入*/
-int linklistAppointPosInser(linklist *pList,int pos, ELEMENTTYPE val);
+int linklistAppointPosInser(linklist *pList,int pos, ELEMENTTYPE val)
+{
+
+}
 
 /*链表头删*/
-int linklistheadDel(linklist *pList);
+int linklistheadDel(linklist *pList)
+{
+
+}
 
 /*链表尾删*/
-int linklisttaiDel(linklist *pList);
+int linklisttaiDel(linklist *pList)
+{
+
+}
 
 /*链表指定位置删*/
-int linklistAppointPosDel(linklist *pList, int pos);
+int linklistAppointPosDel(linklist *pList, int pos)
+{
+
+}
 
 /* 链表删除指定的数据 */
-int LinkListDelAppointData(linklist *pList, ELEMENTTYPE val);
+int LinkListDelAppointData(linklist *pList, ELEMENTTYPE val)
+{
+
+}
 
 /*获取链表的长度*/
-int linklistGetlenght(linklist *pList, int *pSize);
+int linklistGetlenght(linklist *pList, int *pSize)
+{
+
+}
 
 /*链表的销毁*/
-int LinkListDestroy(linklist *pList);
+int LinkListDestroy(linklist *pList)
+{
+    
+}
