90c3699a63b3b68c6acbae5bf9de3bc1689031b1
woyaogaoshiqing <402824251@qq.com>
2023-12-20 10:14:42 +0800

    [修改]完成函数的形参，并在源文件定义

--- linklist.c
+++ linklist.c
@@ -4,3 +4,31 @@
 #include <stdio.h>
 
 
+int linklistInit( linklist **pList);
+
+/*头插*/
+int linklistheadInser(linklist *pList, ELEMENTTYPE val);
+
+/*尾插*/
+int linklisttaiInser(linklist *pList, ELEMENTTYPE val);
+
+/*链表指定位置插入*/
+int linklistAppointPosInser(linklist *pList,int pos, ELEMENTTYPE val);
+
+/*链表头删*/
+int linklistheadDel(linklist *pList);
+
+/*链表尾删*/
+int linklisttaiDel(linklist *pList);
+
+/*链表指定位置删*/
+int linklistAppointPosDel(linklist *pList, int pos);
+
+/* 链表删除指定的数据 */
+int LinkListDelAppointData(linklist *pList, ELEMENTTYPE val);
+
+/*获取链表的长度*/
+int linklistGetlenght(linklist *pList, int *pSize);
+
+/*链表的销毁*/
+int LinkListDestroy(linklist *pList);
--- linklist.h
+++ linklist.h
@@ -27,32 +27,32 @@ typedef struct linklist
     int len;
 }linklist;
 /*链表初始化*/
-int linklistInit();
+int linklistInit( linklist **pList);
 
 /*头插*/
-int linklistheadInser();
+int linklistheadInser(linklist *pList, ELEMENTTYPE val);
 
 /*尾插*/
-int linklisttaiInser();
+int linklisttaiInser(linklist *pList, ELEMENTTYPE val);
 
 /*链表指定位置插入*/
-int linklistAppointPosInser();
+int linklistAppointPosInser(linklist *pList,int pos, ELEMENTTYPE val);
 
 /*链表头删*/
-int linklistheadDel();
+int linklistheadDel(linklist *pList);
 
 /*链表尾删*/
-int linklisttaiDel();
+int linklisttaiDel(linklist *pList);
 
 /*链表指定位置删*/
-int linklistAppointPosDel();
+int linklistAppointPosDel(linklist *pList, int pos);
 
 /* 链表删除指定的数据 */
-int LinkListDelAppointData();
+int LinkListDelAppointData(linklist *pList, ELEMENTTYPE val);
 
 /*获取链表的长度*/
-int linklistGetlenght();
+int linklistGetlenght(linklist *pList, int *pSize);
 
 /*链表的销毁*/
-int LinkListDestroy();
+int LinkListDestroy(linklist *pList);
 #endif  //__LINKLIST_H_
